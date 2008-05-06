a27dc7e3d2829c60e928579d3b65935b982c4110
Jamie Brough <jb@tempestattack.com>
2008-05-06 18:13:33 -0400

    inventory default effective duration

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -2,4 +2,14 @@ class Inventory < ActiveRecord::Base
 
   belongs_to :billable, :polymorphic => true
 
+  after_validation :set_effective_duration
+
+
+  private
+
+  def set_effective_duration
+    self.effective_at ||= Time.now
+    self.expires_at ||= 10.years.from_now
+  end
+
 end
