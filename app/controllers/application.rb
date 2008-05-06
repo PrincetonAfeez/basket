f57c3ad3fc1f9d770c7371d2f8f9707e91703ba4
Jamie Brough <jb@tempestattack.com>
2008-05-06 17:46:01 -0400

    puts cart methods where they belong

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,27 +1,12 @@
-# Filters added to this controller apply to all controllers in the application.
-# Likewise, all the methods added will be available for all controllers.
-
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   before_filter :basket
-  # See ActionController::RequestForgeryProtection for details
-  # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => 'b3d264b155a26dba719323209032beed'
+
+  protect_from_forgery 
 
   
   # Shopping basket 
   
-  def basket_puts(id)
-    apple = Apple.new(id,session[:basket])
-    session[:basket].merge!(apple.crunch)
-  end
-
-  def basket_takes(id)
-    session[:basket][id.to_i] -= 1
-    session[:basket].delete_if {|k,v| v == 0} 
-  end
-
-
   def basket
     session[:basket] ||= {}
     @basket = Basket.new(session[:basket])
--- app/controllers/basket_controller.rb
+++ app/controllers/basket_controller.rb
@@ -1,7 +1,6 @@
 class BasketController < ApplicationController
 
 
-
   def create
     if basket_puts(params[:id])
       flash[:notice] = "Item Added" 
@@ -15,5 +14,20 @@ class BasketController < ApplicationController
       redirect_to :back
     end
   end
+
+
+  private
+
+  def basket_puts(id)
+    apple = Apple.new(id,session[:basket])
+    session[:basket].merge!(apple.crunch)
+  end
+
+  def basket_takes(id)
+    session[:basket][id.to_i] -= 1
+    session[:basket].delete_if {|k,v| v == 0} 
+  end
+
+
 
 end
